dc9613930aeef420405ccfa27de64c095dd628b5
Kartik Chinda <kartik.chinda@gmail.com>
2024-12-04 00:54:17 +0530

    Feat: added contact

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -36,16 +36,35 @@ const Contact = () => {
       >
         <Link
           href={
-            ""
+            "mailto:shreya.singh@uni-bamberg.de"
           }
           target="_blank"
           className="absolute font-black font-hero-heading text-5xl text-center mt-2 md:mt-0 md:text-8xl text-white  tracking-wide"
         >
           Contact me
         </Link>
-        <div className="absolute font-semibold z-10 bottom-10  font-subtext-heebo">
-        
+        <div className="absolute font-semibold z-10 mt-32 text-gray-300 font-subtext-mont text-sm flex justify-center items-center flex-col md:flex-row">
+          <Link href={"mailto:shreya.singh@uni-bamberg.de"} className="text-white font-bold text-base">
+          shreya.singh@uni-bamberg.de
+          </Link> &nbsp; or &nbsp;
+          <Link href={"mailto:shreyasingh.econ@gmail.com"} className="text-white font-bold  text-base">
+          shreyasingh.econ@gmail.com
+          </Link>
         </div>
+
+        <div className="absolute font-semibold z-10 bottom-4 text-gray-100 font-subtext-mont text-sm flex justify-center md:justify-around w-full items-center flex-col md:flex-row gap-8 md:gap-0">
+          <div className="flex flex-col text-center md:text-left hover:text-white duration-300 hover:font-bold">
+          <div>Office Address</div>
+          <div>Room: FG1/00.04, </div>
+          <div>Bamberg Graduate School for Social Sciences</div>
+          <div>Feldkirchenstr. 21,</div>
+          <div>96052 Bamberg, Germany</div>
+          </div>
+          <div className="hover:text-white duration-300 hover:font-bold">
+          Phone: +49(0)951/863-2535
+          </div>
+        </div>
+       
       </div>
     </section>
   );
